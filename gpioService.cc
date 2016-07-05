9144e23d3d63b82a17f7e9c03d7403f11f5215fc
sam <samuel.tygarwen.co.uk>
2016-07-05 11:01:04 +0100

    changed to object generator

--- gpioService.cc
+++ gpioService.cc
@@ -1,67 +1,10 @@
 #include <node.h>
-#include <stdio.h>
-#include <wiringPi.h>
-#include <iostream>
-#define	DELAY	20000
+#include "myobject.h"
 
 using namespace v8;
-using namespace std;
 
-Handle<Value> Write(const Arguments& args) {
-  HandleScope scope;
-
-  if (args.Length() < 2) {
-    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-    return scope.Close(Undefined());
-  }
-
-  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
-    ThrowException(Exception::TypeError(String::New("Wrong argument types")));
-    return scope.Close(Undefined());
-  }
-
-  double port = args[0]->NumberValue();
-  double value = args[1]->NumberValue();
-
-  if(port>12 || port<0){
-    ThrowException(Exception::TypeError(String::New("Invlaid port Value")));
-    return scope.Close(Undefined());
-  }
-  
-  if (value == 1){
-    digitalWrite (0, HIGH);
-  }
-  else if(value == 0) {
-    digitalWrite (0, LOW);
-  }
-  else{
-    ThrowException(Exception::TypeError(String::New("Invlaid write Value")));
-  }
-  return scope.Close(Undefined());
+void InitAll(Handle<Object> exports) {
+  MyObject::Init(exports);
 }
 
-Handle<Value> CreateObject(const Arguments& args)
-{
-  HandleScope scope;
-
-  Local<Object> obj = Object::New();
-  obj->Set(String::NewSymbol("msg"), args[0]->ToString());
-
-  return scope.Close(obj);
-}
-
-void Init(Handle<Object> exports, Handle<Object> module) {
-  wiringPiSetup () ;
-
-  int i;
-  for(i=0;i<13;i++)
-  {
-    pinMode (i, OUTPUT) ;
-  }
-
-  exports->Set(String::NewSymbol("write"), FunctionTemplate::New(Write)->GetFunction());
-  module->Set(String::NewSymbol("exports"), FunctionTemplate::New(CreateObject)->GetFunction());
-}
-
-
-NODE_MODULE(gpioService, Init)
+NODE_MODULE(addon, InitAll)
--- gpioService.js
+++ gpioService.js
@@ -1,10 +1,14 @@
 var gpioService = require('bindings')('gpioService.node')
 
-var a = gpioService("name1")
-console.log(a.msg)
+var obj = new addon.MyObject(10);
+console.log( obj.plusOne() ); // 11
+console.log( obj.plusOne() ); // 12
+console.log( obj.plusOne() ); // 13
 
-var stdin = process.openStdin()
-stdin.addListener("data", function(d){
-  gpioService.write(0, parseInt(d.toString().trim()) )
-})
+console.log( obj.multiply().value() ); // 13
+console.log( obj.multiply(10).value() ); // 130
+
+var newobj = obj.multiply(-1);
+console.log( newobj.value() ); // -13
+console.log( obj === newobj ); // false
 
