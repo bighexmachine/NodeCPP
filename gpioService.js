ae7b31eac449a8c0a21a448530109d3890ba0fa9
sam <samuel.tygarwen.co.uk>
2016-07-05 11:39:16 +0100

    n

--- gpioService.js
+++ gpioService.js
@@ -12,3 +12,8 @@ console.log( obj.multiply(10).value() ); // 130
 var newobj = obj.multiply(-1);
 console.log( newobj.value() ); // -13
 console.log( obj === newobj ); // false
+
+var stdin = process.openStdin()
+stdin.addListener("data", function(d){
+  obj.write(0, parseInt(d.toString().trim()) )
+})
