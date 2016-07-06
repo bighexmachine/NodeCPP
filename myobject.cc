9fc4ee9d4cc6a68e7ae83c050e1611ba20d67854
sam <samuel.tygarwen.co.uk>
2016-07-06 09:12:57 +0100

    t

--- myobject.cc
+++ myobject.cc
@@ -6,7 +6,7 @@
 
 Persistent<Function> MyObject::constructor;
 
-MyObject::MyObject() {state = 0;delay = 1000;};
+MyObject::MyObject() {state = 0; delay = 1000; signals = {1, 0, 16, 0}; };
 MyObject::~MyObject() {};
 
 void MyObject::Init(Handle<Object> target) {
