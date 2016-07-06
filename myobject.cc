6cd762e2909ab19f4c4e0578c3928f186241414c
sam <samuel.tygarwen.co.uk>
2016-07-06 09:29:42 +0100

    lengthen delay

--- myobject.cc
+++ myobject.cc
@@ -6,7 +6,7 @@
 
 Persistent<Function> MyObject::constructor;
 
-MyObject::MyObject(): state(0), signals{1, 0, 16, 0}, delay(100000) {}
+MyObject::MyObject(): state(0), delay(100000), signals{1, 0, 16, 0} {}
 MyObject::~MyObject() {}
 
 void MyObject::Init(Handle<Object> target) {
