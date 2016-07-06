4ba4d1d7213a5e28a98238f292c2145de3d7fc83
sam <samuel.tygarwen.co.uk>
2016-07-06 10:00:45 +0100

    node funcitons

--- gpioService.js
+++ gpioService.js
@@ -7,12 +7,16 @@ stdin.addListener("data", function(d){
 
   if ( d.toString().trim() == "start" ){
       obj.startClock();
+      console.log("started");
   } else if ( d.toString().trim() == "stop" ){
       obj.stopClock();
+      console.log("stopped");
   } else if ( d.toString().trim() == "step" ){
       obj.stepClock();
+      console.log("stepped");
   } else if ( d.toString().trim() == "reset" ){
       obj.reset();
+      console.log("reset");
   }
   //obj.write(0, parseInt(d.toString().trim()) )
 })
